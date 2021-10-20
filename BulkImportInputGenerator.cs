b964eb95b6c11cdc0a6d69f5f2a91b95e330e004
Abhinav Ghai <abhinavghai12290@gmail.com>
2021-10-19 18:10:12 -0700

    Add ADT Bulk Import Converter tool
    ADT Bulk Import Converter tool is a .NET Core tool that takes DTDL models in a directory (1 JSON model per file) and recursively iterates through the directory structure to generate 1 output file in NDJSON format that can be used for providing input bulk import of models to Azure Digital Twin service.
    
    Usage: ADTBulkImportConverter.exe "Input Folder Path" "OutputFileName.ndjson"

---
+++ BulkImportInputGenerator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ADTBulkImportConverter
+{
+    public class BulkImportInputGenerator
+    {
+        public void GenerateBulkModelImportInputFile(string inputPath, string outputFileName)
+        {
+            if (string.IsNullOrWhiteSpace(inputPath))
+            {
+                throw new ArgumentException("Input path is not valid");
+            }
+
+            if (string.IsNullOrWhiteSpace(outputFileName))
+            {
+                outputFileName = "output.ndjson";
+            }
+
+            if (!Directory.Exists(inputPath))
+            {
+                throw new ArgumentException("Input path must be a valid directory");
+            }
+
+            using (var inputStream = new MemoryStream())
+            {
+                using (var inputStreamWriter = new StreamWriter(inputStream))
+                {
+                    ProcessDirectory(inputPath, inputStreamWriter);
+                    inputStream.Position = 0;
+
+                    using (var outputStreamWriter = new StreamWriter(outputFileName))
+                    {
+                        outputStreamWriter.WriteLine(@"{""Section"": ""Header""}");
+                        outputStreamWriter.WriteLine(@"{""fileVersion"": ""1.0.0"", ""author"": ""foobar"", ""organization"": ""contoso""}");
+                        outputStreamWriter.WriteLine(@"{""Section"": ""Models""}");
+                        outputStreamWriter.Flush();
+                        inputStream.CopyTo(outputStreamWriter.BaseStream);
+                    }
+                }
+            }
+        }
+
+        private void ProcessDirectory(string inputPath, StreamWriter streamWriter)
+        {
+            var files = Directory.GetFiles(inputPath);
+            foreach (var file in files)
+            {
+                var fileContent = GetValidJsonFileContent(file);
+                if (fileContent.Length > 0)
+                {
+                    streamWriter.WriteLine(fileContent);
+                    streamWriter.Flush();
+                }
+            }
+
+            var subDirectories = Directory.GetDirectories(inputPath);
+            foreach (var subDirectory in subDirectories)
+            {
+                ProcessDirectory(subDirectory, streamWriter);
+            }
+        }
+
+        private string GetValidJsonFileContent(string filePath)
+        {
+            var jsonString = File.ReadAllText(filePath);
+            try
+            {
+                var json = JToken.Parse(jsonString);
+                return json.ToString(Formatting.None);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid JSON at path: " + filePath);
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ADTBulkImportConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: ADTBulkImportConverter <InputFolderPath> <OutputFileName>");
+                return;
+            }
+
+            var generator = new BulkImportInputGenerator();
+            var inputPath = args[0];
+            try
+            {
+                generator.GenerateBulkModelImportInputFile(inputPath, args[1]);
+                Console.WriteLine("Done!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
